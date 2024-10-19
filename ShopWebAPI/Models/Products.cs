868847e97801a2e2ebdfce012b2a1c94db6d9591
Kenier202 <kenier202@gmail.com>
2024-10-18 22:18:20 -0500

    feat(Products): create products model class

---
+++ ShopWebAPI/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿namespace ShopWebAPI.Models
+{
+    public class Products
+    {
+        public int IdProduct { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductDescription { get; set; }
+        public int StockQuantity { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+
+}
