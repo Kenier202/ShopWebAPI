34799558fe8a509a6f8269a2cb6b6dfb0ab95f70
Kenier202 <kenier202@gmail.com>
2024-10-20 18:18:36 -0500

    feat(ShopWebInsertDTO): add ShopWebInsertDTO

---
+++ ShopWebAPI/DTOs/ShopWebInsertDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ShopWebAPI.DTOs
+{
+    public class ShopWebInsertDTO
+    {
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductCategory { get; set; }
+        public string ProductDescription { get; set; }
+        public int StockQuantity { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
