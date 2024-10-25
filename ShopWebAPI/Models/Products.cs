27cd21fa1f7940678ee6f509eb007ced5d4e7912
Kenier202 <kenier202@gmail.com>
2024-10-24 21:20:18 -0500

    feat(entityFramework): Added data annotations for primary key and required constraints on Products entity

--- ShopWebAPI/Models/Products.cs
+++ ShopWebAPI/Models/Products.cs
@@ -1,14 +1,22 @@
-﻿namespace ShopWebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopWebAPI.Models
 {
     public class Products
     {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IdProduct { get; set; }
         public string ProductName { get; set; }
         public double ProductPrice { get; set; }
         public string ProductCategory { get; set; }
         public string ProductDescription { get; set; }
         public int StockQuantity { get; set; }
-        public int IsActive { get; set; }
+        public int StateProductId { get; set; }
+        [ForeignKey(nameof(StateProductId))]
+        public State_product StateProduct { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime ModifiedDate { get; set; }
     }
--- ShopWebAPI/Models/State_product.cs
+++ ShopWebAPI/Models/State_product.cs
@@ -1,8 +1,16 @@
-﻿namespace ShopWebAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopWebAPI.Models
 {
     public class State_product
     {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public String Estado { get; set; }
+        public ICollection<Products> Products { get; set; }
+
     }
 }
