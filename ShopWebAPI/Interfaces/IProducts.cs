b21843d3dfd99af060d08fca80efae843b9db3cb
Kenier202 <kenier202@gmail.com>
2024-10-24 21:32:41 -0500

    feat(Products): add products interface

---
+++ ShopWebAPI/Interfaces/IProducts.cs
@@ -0,0 +1,19 @@
+﻿using ShopWebAPI.DTOs;
+using ShopWebAPI.Models;
+
+namespace ShopWebAPI.Interfaces
+{
+    public interface IProducts
+    {
+        //get all products from table products
+        Task<IEnumerable<Products>> GetAllProducts();
+        //get product by id
+        Task<Products> GetProductById (int id);
+        //add new product
+        Task<ShopWebInsertDTO> AddProduct (ShopWebInsertDTO shopWebInsertDTO);
+        //update a product
+        Task<ShopWebUpdateDTO> UpdateProduct (ShopWebUpdateDTO shopWebUpdateDTO);
+        //delete a product 
+        Task<Products> DeleteProduct (int id);
+    }
+}
