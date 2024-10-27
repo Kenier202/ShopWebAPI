fabd5d12823a8218a321a37a8ab1dce3e7f141c5
Kenier202 <kenier202@gmail.com>
2024-10-26 22:31:51 -0500

    feat(product repository): add product repository

---
+++ ShopWebAPI/Repositories/ShopWebRepository.cs
@@ -0,0 +1,14 @@
+﻿using ShopWebAPI.DTOs;
+
+namespace ShopWebAPI.Repositories
+{
+    public interface ShopWebRepository<TEntity>
+    {
+        public Task<IEnumerable<TEntity>> getAllProducts();
+        public Task<TEntity> GetProductById();
+        public Task AddProduct (TEntity product);
+        public Task UpdateProduct (TEntity product);
+        public Task DeleteProduct (TEntity product);
+
+    }
+}
