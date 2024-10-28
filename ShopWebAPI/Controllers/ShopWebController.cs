8ce5998bcc59028abd169033bab659b0f81b395e
Kenier202 <kenier202@gmail.com>
2024-10-27 21:02:31 -0500

    feat(ShopWebController): add HttpGet method to get all products

--- ShopWebAPI/Controllers/ShopWebController.cs
+++ ShopWebAPI/Controllers/ShopWebController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using ShopWebAPI.DTOs;
+using ShopWebAPI.Interfaces;
+using ShopWebAPI.Models;
 
 namespace ShopWebAPI.Controllers
 {
@@ -7,5 +10,16 @@ namespace ShopWebAPI.Controllers
     [ApiController]
     public class ShopWebController : ControllerBase
     {
+        private IProductsService _productsService;
+        public ShopWebController(IProductsService productsService) 
+        {
+            _productsService = productsService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Products>> getProducts() => await _productsService.GetProducts();
+
+   
+
     }
 }
