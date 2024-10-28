a1a841cc04a2e3726d279029713a257837de94a5
Kenier202 <kenier202@gmail.com>
2024-10-27 21:00:59 -0500

    feat(IProductsService): create implement of IProductsService interface

--- ShopWebAPI/Services/ProductsService.cs
+++ ShopWebAPI/Services/ProductsService.cs
@@ -1,11 +1,24 @@
 ﻿using ShopWebAPI.DTOs;
 using ShopWebAPI.Interfaces;
 using ShopWebAPI.Models;
+using ShopWebAPI.Repositories;
 
 namespace ShopWebAPI.Services
 {
     public class ProductsService : IProductsService
     {
+        IProductsRepository<Products> _repositoryProducts;
+        public ProductsService (IProductsRepository<Products> repositoryProducts)
+        {
+            _repositoryProducts = repositoryProducts;
+        }
+
+        public Task<IEnumerable<Products>> GetProducts()
+        {
+            var products = _repositoryProducts.getAllProducts();
+
+            return products;
+        }
         public Task<ShopWebDTO> AddProduct(ShopWebInsertDTO product)
         {
             throw new NotImplementedException();
@@ -16,10 +29,7 @@ namespace ShopWebAPI.Services
             throw new NotImplementedException();
         }
 
-        public Task<IEnumerable<Products>> GetProducts()
-        {
-            throw new NotImplementedException();
-        }
+
 
         public Task<Products> GetProductsById(int id)
         {
