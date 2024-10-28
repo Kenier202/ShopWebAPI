9067ae787ba56b10674bc4e6053e92cf1fe5f6ca
Kenier202 <kenier202@gmail.com>
2024-10-27 20:27:29 -0500

    feat(ProductsService): implement IproductsService interface

---
+++ ShopWebAPI/Services/ProductsService.cs
@@ -0,0 +1,34 @@
+﻿using ShopWebAPI.DTOs;
+using ShopWebAPI.Interfaces;
+using ShopWebAPI.Models;
+
+namespace ShopWebAPI.Services
+{
+    public class ProductsService : IProductsService
+    {
+        public Task<ShopWebDTO> AddProduct(ShopWebInsertDTO product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Products> DeleteProduct(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Products>> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Products> GetProductsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ShopWebDTO> UpdateProduct(ShopWebInsertDTO product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
