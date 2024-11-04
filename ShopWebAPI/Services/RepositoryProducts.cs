b7b9171cb0fc1bf83253838e406f9aaef64dd4aa
Kenier202 <kenier202@gmail.com>
2024-11-04 13:40:51 -0500

    feat(RepositoryProducts): add implementation to update products

--- ShopWebAPI/Services/RepositoryProducts.cs
+++ ShopWebAPI/Services/RepositoryProducts.cs
@@ -21,17 +21,27 @@ namespace ShopWebAPI.Services
 
 
         public async Task<Products> GetProductById(int IdProduct) => await _dbContext.Products.FindAsync(IdProduct);
+
         public async Task AddProduct(Products product)
         {
             _dbContext.Products.Add(product);
             await _dbContext.SaveChangesAsync();  // Guarda los cambios en la base de datos de manera asíncrona
         }
-        public Task UpdateProduct(Products product)
+
+        public async Task UpdateProduct(Products product)
         {
-            throw new NotImplementedException();
+            _dbContext.Products.Attach(product);
+            _dbContext.Entry(product).State = EntityState.Modified;
+
+            await _dbContext.SaveChangesAsync();
         }
 
-        public async Task DeleteProduct(Products product) =>  _dbContext.Remove(product);
+        public async Task DeleteProduct(Products product)
+        {
+            _dbContext.Remove(product);
+            await _dbContext.SaveChangesAsync();  // Guarda los cambios en la base de datos de manera asíncrona
+        }
+
         public async Task save() => await _dbContext.SaveChangesAsync();
 
     }
