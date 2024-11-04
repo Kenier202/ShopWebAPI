b9c8ce52e0865bcdb1e667678f46c4e8d8e573be
Kenier202 <kenier202@gmail.com>
2024-11-04 13:42:08 -0500

    feat(WebController): add endpoint to update product and delete

--- ShopWebAPI/Controllers/ShopWebController.cs
+++ ShopWebAPI/Controllers/ShopWebController.cs
@@ -11,7 +11,7 @@ namespace ShopWebAPI.Controllers
     public class ShopWebController : ControllerBase
     {
         private IProductsService _productsService;
-        public ShopWebController(IProductsService productsService) 
+        public ShopWebController(IProductsService productsService)
         {
             _productsService = productsService;
         }
@@ -20,13 +20,33 @@ namespace ShopWebAPI.Controllers
         public async Task<ActionResult> getProducts() => await _productsService.GetProducts() is var product && product == null ? NotFound() : Ok(product);
 
         [HttpGet("{id}")]
-        public async Task<ActionResult> getById(int id ) => 
+        public async Task<ActionResult> getById(int id) =>
             await _productsService.GetProductsById(id) is var product && product == null ? NotFound() : Ok(product);
 
         [HttpPost]
 
         public async Task<ActionResult> addProduct(ShopWebInsertDTO productAdd) =>
-            await _productsService.AddProduct(productAdd) is var addProduct && addProduct == null ? NotFound() : Ok(addProduct);
+            await _productsService.AddProduct(productAdd) is var addProduct && addProduct == null ? NotFound() : Created();
+
+        [HttpDelete("{id}")]
+
+        public async Task<ActionResult> deleteProduct(int id) => await _productsService.DeleteProduct(id)
+            is var product && product == null ? NotFound() : Ok(product);
+
+        [HttpPut("{id}")]
+
+        public async Task<ActionResult> updateProduct(int id, ShopWebUpdateDTO product)
+        {
+            if (id != product.IdProduct) return BadRequest();
+
+            var result = await _productsService.UpdateProduct(product);
+
+            if (result == null) return NotFound();
+
+            return Ok(result);
+        }
+
+
 
 
 
