36413ad7bf1c154f36b55c8c4220d875603f6c1f
Kenier202 <kenier202@gmail.com>
2024-11-04 14:23:19 -0500

    feat(AutoMapper): create auto mapper profile

---
+++ ShopWebAPI/Mappers/AutoMapper.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using ShopWebAPI.DTOs;
+using ShopWebAPI.Models;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile() 
+    {
+        CreateMap<Products, ShopWebDTO>();
+        CreateMap<ShopWebInsertDTO, ShopWebDTO>();
+        CreateMap<ShopWebInsertDTO, Products>();
+        CreateMap<ShopWebUpdateDTO, Products>();
+
+
+    }
+}
